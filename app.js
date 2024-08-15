ae0ee1fa5293e90456c1d69da2768ce931632011
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-08-16 00:13:21 +0530

    HTML Changes

---
+++ app.js
@@ -0,0 +1,13 @@
+const base_URL = "http://api.weatherapi.com/v1/current.json?key=97b3135bfa2c4e3faad181503241508&q=London&aqi=no";
+
+let input = document.querySelector("input");
+let search = document.querySelector(".search-icon");
+
+
+search.addEventListener("click", () => {
+    if(input.value = ""){
+        console.log("Enter City Name");
+    } else {
+        console.log(input.value);
+    }
+})
