267d9668f40d85e90ec9f837b2e405dc58a1c4f1
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-08-17 01:03:39 +0530

    Js Code advancement

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const base_URL = "http://api.weatherapi.com/v1/current.json?key=97b3135bfa2c4e3f
 let input = document.querySelector("input");
 let search = document.querySelector(".search-icon");
 
-
-search.addEventListener("click", () => {
-    var city = input.value;
-
-})
+async function checkweather() {
+    const response = await fetch(base_URL)   ;
+    var data = await response.json();
+    console.log(data.current);
+    
+    document.querySelector(".city-temp").innerText = Math.round(data.current.temp_c) + "°C";
+    document.querySelector("#humidity-here").innerText = data.current.humidity + "%"
+    document.querySelector("#speed-here").innerText = data.current.wind_kph + " Km/h";
+}
