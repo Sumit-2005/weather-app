d6030cba18f0151fe5ab380c4c2f834879806fe7
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-08-17 14:33:55 +0530

    Project Complete

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const base_URL = "http://api.weatherapi.com/v1/current.json?key=97b3135bfa2c4e3faad181503241508&q=London&aqi=no";
+const base_URL = "http://api.weatherapi.com/v1/current.json?key=97b3135bfa2c4e3faad181503241508&q=";
 
 let input = document.querySelector("input");
 let search = document.querySelector(".search-icon");
 
-async function checkweather() {
-    const response = await fetch(base_URL)   ;
+async function checkweather(city) {
+    const response = await fetch(base_URL + city + "&aqi=no");
     var data = await response.json();
-    console.log(data.current);
     
     document.querySelector(".city-temp").innerText = Math.round(data.current.temp_c) + "°C";
     document.querySelector("#humidity-here").innerText = data.current.humidity + "%"
     document.querySelector("#speed-here").innerText = data.current.wind_kph + " Km/h";
+    document.querySelector(".city-name").innerText = city + ", " + data.current.condition.text;
+    document.querySelector("#wethr-icon").src = data.current.condition.icon;
 }
+
+search.addEventListener("click", () => {
+    checkweather(input.value);
+    input.value = "";
+})
